b7f9d055d7572d784c84a96775804fb5ee41fd19
Darío Javier Cravero <dario@qinnova.com.ar>
2011-11-23 19:52:22 +0000

    Sliced login box. A few css effects missing but generally speaking it's there

--- views/index.js
+++ views/index.js
@@ -2,5 +2,11 @@ PAd.Views.Index = Backbone.View.extend({
   el: '#PAd .container',
   render: function() {
     $(this.el).html(_.jst('index'));
+    $('.session').popover({trigger: "manual", placement: 'above',
+      offset: 10,
+      html: true,
+      content: function(){return _.jst('login')({});},
+      template: _.jst('session_popover')({})});
+    $('.session').popover('show');
   }
 });
